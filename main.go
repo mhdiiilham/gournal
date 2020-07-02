ae6596f2508308829b566cdd772b3aadad038318
mhdiiilham <muhd.iiilham@gmail.com>
2020-07-03 04:30:51 +0700

    Fixed typo when invoking Router() and create function called migrate at main.go

--- main.go
+++ main.go
@@ -9,7 +9,15 @@ import (
 
 func main() {
 	db.DB()
-	r := routers.Routaer()
+	r := routers.Router()
+	migrate()
 	log.Info("SERVER IS RUNNING")
 	r.Run(":8000")
+}
+
+// Migrate ...
+func migrate() {
+	log.Info("Start migration")
+	db.DB().AutoMigrate(models.Admin{}, models.Journal{}, models.Image{})
+	log.Info("Migration done!")
 }
