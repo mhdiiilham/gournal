5b6209c9e7e0c0cb27e05540108bf4f2df92db4b
mhdiiilham <muhd.iiilham@gmail.com>
2020-07-15 02:00:20 +0700

    Read file from os.Getenv
    before:
    	r.Run(":8000")
    
    after:
    	// added import "os"
    	r.Run(os.Getenv("GO_PORT"))

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-contrib/cors"
 	"github.com/mhdiiilham/gournal/models"
 	"github.com/mhdiiilham/gournal/routers"
@@ -12,5 +14,5 @@ func main() {
 	r := routers.Router()
 	r.Use(cors.Default())
 	log.Info("SERVER IS RUNNING")
-	r.Run(":8000")
+	r.Run(os.Getenv("GO_PORT"))
 }
