282eb621bf8682da0596ece7c43b9866bc3153a0
mhdiiilham <muhd.iiilham@gmail.com>
2020-07-15 01:44:45 +0700

    Refactoring connection to Database

--- main.go
+++ main.go
@@ -1,23 +1,14 @@
 package main
 
 import (
-	"github.com/mhdiiilham/gournal/db"
 	"github.com/mhdiiilham/gournal/models"
 	"github.com/mhdiiilham/gournal/routers"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	db.DB()
+	models.ConnectDatabase()
 	r := routers.Router()
-	migrate()
 	log.Info("SERVER IS RUNNING")
 	r.Run(":8000")
-}
-
-// Migrate ...
-func migrate() {
-	log.Info("Start migration")
-	db.DB().AutoMigrate(models.Admin{}, models.Journal{}, models.Image{})
-	log.Info("Migration done!")
 }
