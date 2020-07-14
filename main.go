d1fffa7d1f247fc5ff36715ff5969f4ef9554f31
mhdiiilham <muhd.iiilham@gmail.com>
2020-07-15 01:57:57 +0700

    Added CORS, (default config: allow all origin)

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/mhdiiilham/gournal/models"
 	"github.com/mhdiiilham/gournal/routers"
 	log "github.com/sirupsen/logrus"
@@ -9,6 +10,7 @@ import (
 func main() {
 	models.ConnectDatabase()
 	r := routers.Router()
+	r.Use(cors.Default())
 	log.Info("SERVER IS RUNNING")
 	r.Run(":8000")
 }
