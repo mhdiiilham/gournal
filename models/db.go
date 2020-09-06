effbf702c2f041066550efd722856acbb35b930b
Muhammad Ilham <muhd.iiilham@gmail.com>
2020-09-06 13:03:53 +0700

    Let's start working on this project again

--- models/db.go
+++ models/db.go
@@ -2,10 +2,10 @@ package models
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/mhdiiilham/gournal/helpers"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -14,15 +14,15 @@ var DB *gorm.DB
 
 // ConnectDatabase ...
 func ConnectDatabase() {
-	host := os.Getenv("MYSQL_HOST")
-	port := os.Getenv("MYSQL_PORT")
-	user := os.Getenv("MYSQL_USER")
-	pass := os.Getenv("MYSQL_USER_PASSWORD")
+	host := helpers.Getenv("MYSQL_HOST")
+	user := helpers.Getenv("MYSQL_USER")
+	pass := helpers.Getenv("MYSQL_USER_PASSWORD")
 	name := "journal"
+	log.Info(host, user, pass, "halloo")
 
 	uri := fmt.Sprintf(
-		"%s:%s@(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		user, pass, host, port, name,
+		"%s:%s@(%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		user, pass, host, name,
 	)
 
 	database, err := gorm.Open("mysql", uri)
